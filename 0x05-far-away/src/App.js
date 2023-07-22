f4acc3d37d643f62a251b35cf78b98a5c08e9c68
Cleve <clevemomanyi@gmail.com>
2023-07-22 16:28:37 +0300

    derived state

--- 0x05-far-away/src/App.js
+++ 0x05-far-away/src/App.js
@@ -33,7 +33,7 @@ export default function App() {
         onDeleteItems={handleDeleteItems}
         onToggle={handleToggle}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -119,10 +119,26 @@ function Item({ item, onDeleteItems, onToggle }) {
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  const numItems = items.length;
+  const numItemsPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.floor((numItemsPacked / numItems) * 100);
+
+  if (!numItems) {
+    return (
+      <p className="stats">
+        <em>Start adding some some items to your packing list</em>
+      </p>
+    );
+  }
+
   return (
     <footer className="stats">
-      <em>🧳 You have X items in your list, and you have packed Y (X%)</em>
+      <em>
+        {percentage === 100
+          ? "You have got everything ready to go."
+          : `You have ${numItems} items in your list, and you have packed ${numItemsPacked} (${percentage}%)`}
+      </em>
     </footer>
   );
 }
