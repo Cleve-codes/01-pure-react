2e19f51232af91c90ac85d3a97c095942f36022a
Cleve <clevemomanyi@gmail.com>
2023-07-23 19:09:39 +0300

    Part 1 almost

--- 0x04-props-and-state/eat-n-split/src/App.js
+++ 0x04-props-and-state/eat-n-split/src/App.js
@@ -1,7 +1,60 @@
-import "./App.css";
+const initialFriends = [
+  {
+    id: 118836,
+    name: "Clark",
+    image: "https://i.pravatar.cc/48?u=118836",
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: "Sarah",
+    image: "https://i.pravatar.cc/48?u=933372",
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: "Anthony",
+    image: "https://i.pravatar.cc/48?u=499476",
+    balance: 0,
+  },
+];
 
-function App() {
-  return <div></div>;
+export default function App() {
+  return (
+    <div className="app">
+      <div className="sidebar">
+        <FriendList />
+      </div>
+    </div>
+  );
 }
 
-export default App;
+function FriendList() {
+  const friends = initialFriends;
+  return (
+    <ul>
+      {friends.map((friend) => (
+        <Friend key={friend.id} friend={friend} />
+      ))}
+    </ul>
+  );
+}
+
+function Friend({ friend }) {
+  return (
+    <li>
+      <img src={friend.image} alt={friend.name} />
+      <h3>{friend.name}</h3>
+      <p className={friend.balance <= 0 ? `green` : `red`}>
+        {friend.balance > 0
+          ? `You owe ${friend.name} ${Math.abs(friend.balance)} $`
+          : `${friend.name} owes you ${Math.abs(friend.balance)} $`}
+      </p>
+      <Button>Select</Button>
+    </li>
+  );
+}
+
+function Button({ children }) {
+  return <button className="button">{children}</button>;
+}
