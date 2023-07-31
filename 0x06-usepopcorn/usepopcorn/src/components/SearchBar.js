0ed834468803efafb6b6d7ee55e8a2226f81becd
Cleve <clevemomanyi@gmail.com>
2023-07-31 20:26:47 +0300

    changes

--- 0x06-usepopcorn/usepopcorn/src/components/SearchBar.js
+++ 0x06-usepopcorn/usepopcorn/src/components/SearchBar.js
@@ -2,24 +2,43 @@ import React from "react";
 import { useState } from "react";
 
 const SearchBar = () => {
-  const [query, setQuery] = useState("");
   return (
     <nav className="nav-bar">
-      <div className="logo">
-        <span role="img">🍿</span>
-        <h1>usePopcorn</h1>
-      </div>
-      <input
-        className="search"
-        type="text"
-        placeholder="Search movies..."
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-      />
-      <p className="num-results">
-        Found <strong>X</strong> results
-      </p>
+      <Logo />
+      <Input />
+      <Results />
     </nav>
+  );
+};
+
+const Logo = () => {
+  return (
+    <div className="logo">
+      <span role="img">🍿</span>
+      <h1>usePopcorn</h1>
+    </div>
+  );
+};
+
+const Input = () => {
+  const [query, setQuery] = useState("");
+
+  return (
+    <input
+      className="search"
+      type="text"
+      placeholder="Search movies..."
+      value={query}
+      onChange={(e) => setQuery(e.target.value)}
+    />
+  );
+};
+
+const Results = () => {
+  return (
+    <p className="num-results">
+      Found <strong>X</strong> results
+    </p>
   );
 };
 
