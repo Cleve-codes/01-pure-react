078ea19931c215af9d8ec5e1189b0c1b619267e3
Cleve <clevemomanyi@gmail.com>
2023-08-01 00:35:33 +0300

    bugged

--- 0x06-usepopcorn/usepopcorn/src/App.js
+++ 0x06-usepopcorn/usepopcorn/src/App.js
@@ -1,8 +1,7 @@
 import { useState } from "react";
 import SearchBar from "./components/SearchBar";
 import Main from "./components/Main";
-import ListBox from "./components/ListBox";
-import WatchedBox from "./components/WatchedBox";
+import Box from "./components/Box";
 
 const tempMovieData = [
   {
@@ -28,8 +27,32 @@ const tempMovieData = [
   },
 ];
 
+const tempWatchedData = [
+  {
+    imdbID: "tt1375666",
+    Title: "Inception",
+    Year: "2010",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+    runtime: 148,
+    imdbRating: 8.8,
+    userRating: 10,
+  },
+  {
+    imdbID: "tt0088763",
+    Title: "Back to the Future",
+    Year: "1985",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
+    runtime: 116,
+    imdbRating: 8.5,
+    userRating: 9,
+  },
+];
+
 export default function App() {
   const [movies, setMovies] = useState(tempMovieData);
+  const [watched, setWatched] = useState(tempWatchedData);
 
   const Logo = () => {
   return (
@@ -72,6 +95,39 @@ const MovieList = ({ movies }) => {
   );
 };
 
+const average = (arr) =>
+  arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
+
+const Summary = ({ watched }) => {
+  const avgImdbRating = average(watched.map((movie) => movie.imdbRating));
+  const avgUserRating = average(watched.map((movie) => movie.userRating));
+  const avgRuntime = average(watched.map((movie) => movie.runtime));
+
+  return (
+    <div className="summary">
+      <h2>Movies you watched</h2>
+      <div>
+        <p>
+          <span>#️⃣</span>
+          <span>{watched.length} movies</span>
+        </p>
+        <p>
+          <span>⭐️</span>
+          <span>{avgImdbRating}</span>
+        </p>
+        <p>
+          <span>🌟</span>
+          <span>{avgUserRating}</span>
+        </p>
+        <p>
+          <span>⏳</span>
+          <span>{avgRuntime} min</span>
+        </p>
+      </div>
+    </div>
+  );
+};
+
 const Movie = ({ movie }) => {
   return (
     <li key={movie.imdbID}>
@@ -95,10 +151,14 @@ const Movie = ({ movie }) => {
         <Results />
       </SearchBar>
       <Main>
-        <ListBox>
+        <Box>
           <MovieList movies={movies} />
-        </ListBox>
-        <WatchedBox movies={movies} />
+        </Box>
+
+        <Box>
+          <Summary />
+        </Box>
+       
       </Main>
     </>
   );
