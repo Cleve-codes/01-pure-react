fc200c9b9158a4a4714fd33ac2311aff0d60ba8d
Cleve <clevemomanyi@gmail.com>
2023-07-31 21:05:03 +0300

    Abstracted

--- 0x06-usepopcorn/usepopcorn/src/components/ListBox.js
+++ 0x06-usepopcorn/usepopcorn/src/components/ListBox.js
@@ -27,7 +27,6 @@ const tempMovieData = [
 
 const ListBox = () => {
   const [isOpen1, setIsOpen1] = useState(true);
-  const [movies, setMovies] = useState(tempMovieData);
   return (
     <div className="box">
       <button
@@ -36,23 +35,35 @@ const ListBox = () => {
       >
         {isOpen1 ? "–" : "+"}
       </button>
-      {isOpen1 && (
-        <ul className="list">
-          {movies?.map((movie) => (
-            <li key={movie.imdbID}>
-              <img src={movie.Poster} alt={`${movie.Title} poster`} />
-              <h3>{movie.Title}</h3>
-              <div>
-                <p>
-                  <span>🗓</span>
-                  <span>{movie.Year}</span>
-                </p>
-              </div>
-            </li>
-          ))}
-        </ul>
-      )}
+      {isOpen1 && <MovieList />}
     </div>
+  );
+};
+
+const MovieList = () => {
+  const [movies, setMovies] = useState(tempMovieData);
+
+  return (
+    <ul className="list">
+      {movies?.map((movie) => (
+        <Movie movie={movie} />
+      ))}
+    </ul>
+  );
+};
+
+const Movie = ({ movie }) => {
+  return (
+    <li key={movie.imdbID}>
+      <img src={movie.Poster} alt={`${movie.Title} poster`} />
+      <h3>{movie.Title}</h3>
+      <div>
+        <p>
+          <span>🗓</span>
+          <span>{movie.Year}</span>
+        </p>
+      </div>
+    </li>
   );
 };
 
