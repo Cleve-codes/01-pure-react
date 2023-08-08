798dc671436e1b3630eccf05158d13f60f7c46f5
Cleve <clevemomanyi@gmail.com>
2023-08-08 23:43:46 +0300

    final

--- 0x07-text-extennder/src/App.js
+++ 0x07-text-extennder/src/App.js
@@ -1,6 +1,5 @@
-import { useState } from "react";
+import TextExpander from "./TextExpander";
 import "./index.css";
-
 
 export default function App() {
   return (
@@ -32,40 +31,6 @@ export default function App() {
         travel is a pretty cool thing to think about. Who knows what we'll
         discover next!
       </TextExpander>
-    </div>
-  );
-}
-
-function TextExpander({
-  collapsedNumWords = 10,
-  expandButtonText = "Show more",
-  collapseButtonText = "Show less",
-  buttonColor = "#1f09cd",
-  expanded = false,
-  className,
-  children,
-}) {
-  const [isExpanded, setIsExpanded] = useState(expanded);
-
-  const displayText = isExpanded
-    ? children
-    : children.split(" ").slice(0, collapsedNumWords).join(" ") + "...";
-
-  const buttonStyle = {
-    background: "none",
-    border: "none",
-    font: "inherit",
-    cursor: "pointer",
-    marginLeft: "6px",
-    color: buttonColor,
-  };
-
- 
-
-  return (
-    <div className={className}>
-      <span>{displayText}</span>
-      <button style={buttonStyle} onClick={()=>setIsExpanded((exp)=>!exp)}>{isExpanded ? expandButtonText : collapseButtonText}</button>
     </div>
   );
 }
