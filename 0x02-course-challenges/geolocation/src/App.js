e61b813dcbc1ac1c66c66934eb6206824bdd0e27
Cleve <clevemomanyi@gmail.com>
2023-08-11 08:31:24 +0300

    solved

--- 0x02-course-challenges/geolocation/src/App.js
+++ 0x02-course-challenges/geolocation/src/App.js
@@ -6,8 +6,9 @@ function useGeolocation() {
   const [error, setError] = useState(null);
 
   function getPosition() {
-    if (!navigator.geolocation)
-      return setError("Your browser does not support geolocation");
+    if (!navigator.geolocation) {
+      return setError("Your browser doesn't support geolocation");
+    }
 
     setIsLoading(true);
     navigator.geolocation.getCurrentPosition(
@@ -24,7 +25,6 @@ function useGeolocation() {
       }
     );
   }
-
   return { isLoading, position, error, getPosition };
 }
 
