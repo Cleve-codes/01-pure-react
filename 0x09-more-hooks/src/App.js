13f425c77461ae273fc29fd08b51caa779e4cd96
Cleve <clevemomanyi@gmail.com>
2023-08-18 14:18:48 +0300

    api

--- 0x09-more-hooks/src/App.js
+++ 0x09-more-hooks/src/App.js
@@ -1,8 +1,20 @@
+import { useEffect } from "react";
 import Header from "./Header"
 import Main from "./Main";
 import "./index.css";
 
 function App() {
+
+  useEffect(()=>{
+     const getData = async () => {
+         const res = await fetch(`http://localhost:8000/questions`);
+         const data = await res.json();
+         console.log(data)
+       };
+
+       getData();
+  }, [])
+
   return (
     <div className="App">
       <Header />
