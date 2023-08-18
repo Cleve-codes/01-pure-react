cd536808fa0ddca1706e524af07f7b7fdc08ded8
Cleve <clevemomanyi@gmail.com>
2023-08-18 14:57:45 +0300

    more reducer

--- 0x09-more-hooks/src/App.js
+++ 0x09-more-hooks/src/App.js
@@ -1,19 +1,44 @@
-import { useEffect } from "react";
-import Header from "./Header"
+import { useEffect, useReducer } from "react";
+import Header from "./Header";
 import Main from "./Main";
 import "./index.css";
 
+const initialState = {
+  question: [],
+
+  // statuses => "loading", "error", "ready", "active", "finished".
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataRecieved":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+
+    default:
+      throw new Error("Data not found");
+  }
+}
+
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
 
-  useEffect(()=>{
-     const getData = async () => {
-         const res = await fetch(`http://localhost:8000/questions`);
-         const data = await res.json();
-         console.log(data)
-       };
-
-       getData();
-  }, [])
+  useEffect(() => {
+    fetch(`http://localhost:8000/questions`)
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataRecieved", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
 
   return (
     <div className="App">
