a43915a0f1ade9accfef16353c69f9f4569e7552
Cleve <clevemomanyi@gmail.com>
2023-08-23 12:17:51 +0300

    yet

--- 0x09-more-hooks/src/components/Option.js
+++ 0x09-more-hooks/src/components/Option.js
@@ -1,28 +1,25 @@
 function Option({ question, dispatch, answer }) {
+  console.log(answer)
   const hasAnswered = answer !== null;
 
   return (
     <div className="options">
-      {question.options.map((q, i) => {
-        return (
-          <button
-            key={i}
-            
-            onClick={() => dispatch({ type: "newAnswer", payload: i })}
-            className={`btn btn-option ${i} === ${
-              q === answer ? "success" : ""
-            } ${
-              hasAnswered
-                ? i === question.correctOption
-                  ? "correct"
-                  : "wrong"
-                : ""
-            }`}
-          >
-            {q}
-          </button>
-        );
-      })}
+      {question.options.map((option, index) => (
+        <button
+          className={`btn btn-option ${index === answer ? "answer" : ""} ${
+            hasAnswered
+              ? index === question.correctOption
+                ? "correct"
+                : "wrong"
+              : ""
+          }`}
+          key={option}
+          disabled={hasAnswered}
+          onClick={() => dispatch({ type: "newAnswer", payload: index })}
+        >
+          {option}
+        </button>
+      ))}
     </div>
   );
 }
