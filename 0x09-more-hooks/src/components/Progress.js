ee71ae7bd78f71814a620f6dbf6a9a55eb7cb4f5
Cleve <clevemomanyi@gmail.com>
2023-08-25 09:58:32 +0300

    mod

--- 0x09-more-hooks/src/components/Progress.js
+++ 0x09-more-hooks/src/components/Progress.js
@@ -1,6 +1,7 @@
 function Progress({ index, numQuestions }) {
   return (
     <header className="progress">
+      <progress max={10} ></progress>
       <p>
         Question {index} / {numQuestions}
       </p>
