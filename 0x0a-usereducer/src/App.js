e99740ff64952c480bac7cef8d4726db65f3da2a
Cleve <clevemomanyi@gmail.com>
2023-09-01 13:30:25 +0300

    debugged

--- 0x0a-usereducer/src/App.js
+++ 0x0a-usereducer/src/App.js
@@ -3,37 +3,72 @@ import Header from "./Header";
 import Bank from "./Bank";
 import { useReducer } from "react";
 
-function reducer(state, action){
-    switch(action.type){
-      case "openAccount":
-        return {
-          ...state, isActive: false
-        }
-      
-      case "closeAccount":
-        return {
-          ...state, isActive: true
-        }
+function reducer(state, action) {
+  switch (action.type) {
+    case "openAccount":
+      return {
+        ...state,
+        isActive: false,
+      };
 
-      default :
-      return "UNKOWN ACTION"
-    }
+    case "closeAccount":
+      return {
+        ...state,
+        isActive: true,
+      };
+
+    case "deposit":
+      return {
+        ...state,
+        balance: state.balance + 150,
+      };
+
+    case "withdraw":
+      return {
+        ...state,
+        balance: state.balance > 0 ? state.balance - 50 : state.balance,
+      };
+
+    case "loan":
+      return {
+        ...state,
+        balance: state.balance + 5000,
+      };
+
+    case "payLoan":
+      return {
+        ...state,
+        loan: 0
+      }
+
+    default:
+      return "UNKOWN ACTION";
+  }
 }
 
 const initialState = {
   balance: 0,
   loan: 0,
   isActive: true,
-}
+};
 
 function App() {
+  const [{ balance, loan, isActive }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
 
-  const [{balance, loan, isActive}, dispatch] = useReducer(reducer, initialState)
-
-  return <div className="App">
-    <Header />
-    <Bank dispatch={dispatch} isActive={isActive} loan={loan} balance={balance} />
-  </div>;
+  return (
+    <div className="App">
+      <Header />
+      <Bank
+        dispatch={dispatch}
+        isActive={isActive}
+        loan={loan}
+        balance={balance}
+      />
+    </div>
+  );
 }
 
 export default App;
--- 0x0a-usereducer/src/Bank.jsx
+++ 0x0a-usereducer/src/Bank.jsx
@@ -1,19 +1,19 @@
-function Bank({ dispatch, isActive }) {
+function Bank({ dispatch, isActive, balance, loan }) {
   return (
     <div>
-      <p>Balance : 0</p>
-      <p>Loan : 0</p>
+      <p>Balance : {balance}</p>
+      <p>Loan : {loan}</p>
       <button onClick={() => dispatch({ type: "openAccount" })}>
         Open Account
       </button>
       <br></br>
-      <button disabled={isActive}>Deposit 150</button>
+      <button onClick={()=>dispatch({type: "deposit"})} disabled={isActive}>Deposit 150</button>
       <br></br>
-      <button disabled={isActive}>Withdraw 50</button>
+      <button onClick={()=>dispatch({type: "withdraw"})} disabled={isActive}>Withdraw 50</button>
       <br></br>
-      <button disabled={isActive}>Request Loan of 5000</button>
+      <button onClick={()=>dispatch({type: "loan"})} disabled={isActive}>Request Loan of 5000</button>
       <br></br>
-      <button disabled={isActive}>Pay loan</button>
+      <button onClick={()=>dispatch({type: "payLoan"})} disabled={isActive}>Pay loan</button>
       <br></br>
       <button onClick={()=>{dispatch({type: "closeAccount"})}} disabled={isActive} >close account</button>
     </div>
