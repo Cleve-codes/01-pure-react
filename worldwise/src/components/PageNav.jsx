04087d13abee52d922b24579f5cf0d8a0077b612
Cleve <clevemomanyi@gmail.com>
2023-09-08 09:40:25 +0300

    NavLink

--- worldwise/src/components/PageNav.jsx
+++ worldwise/src/components/PageNav.jsx
@@ -1,21 +1,21 @@
-import { Link } from "react-router-dom"
+import { NavLink } from "react-router-dom"
 
 function PageNav() {
     return (
       <nav>
         <ul>
           <li>
-            <Link to="/">Home</Link>
+            <NavLink to="/">Home</NavLink>
           </li>
           <li>
-            <Link to="/pricing">Pricing</Link>
+            <NavLink to="/pricing">Pricing</NavLink>
           </li>
           <li>
-            <Link to="/product">Product</Link>
+            <NavLink to="/product">Product</NavLink>
           </li>
         </ul>
       </nav>
     );
 }
 
-export default PageNav
+export default PageNav;
