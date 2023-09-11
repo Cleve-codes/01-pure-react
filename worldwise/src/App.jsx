4489d39a50bb1afdc6b8a35d73fa697be42770f3
Cleve <clevemomanyi@gmail.com>
2023-09-11 15:13:55 +0300

    pagenav

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -10,9 +10,9 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Homepage />} />
-        <Route path="product" element={<Product />} />
-        <Route path="pricing" element={<Pricing />} />
-        <Route path="app" element={<AppLayout />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/app" element={<AppLayout />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
