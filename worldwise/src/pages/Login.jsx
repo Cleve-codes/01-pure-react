bbc6866789d1e4108ee83be1242234ef7b2b7183
Cleve <clevemomanyi@gmail.com>
2023-09-12 09:38:19 +0300

    pagenav

--- worldwise/src/pages/Login.jsx
+++ worldwise/src/pages/Login.jsx
@@ -1,4 +1,5 @@
 import styles from "./Login.module.css";
+import PageNav from "../components/PageNav"
 import { useState } from 'react'
 
 export default function Login() {
@@ -8,6 +9,7 @@ export default function Login() {
 
   return (
     <main className={styles.login}>
+      <PageNav />
       <form className={styles.form}>
         <div className={styles.row}>
           <label htmlFor="email">Email address</label>
