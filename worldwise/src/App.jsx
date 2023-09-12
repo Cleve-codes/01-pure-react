e04502fbb7298ea8b79f48181f26d5b0783f5eb5
Cleve <clevemomanyi@gmail.com>
2023-09-12 10:31:09 +0300

    index elem

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -14,11 +14,18 @@ function App() {
         <Route path="/product" element={<Product />} />
         <Route path="/pricing" element={<Pricing />} />
         <Route path="/login" element={<Login />} />
+
+        {/* Nested Routes */}
         <Route path="/app" element={<AppLayout />}>
+          {/* Specifying the Index element which would
+          be shown by default incase no path is specified */}
+          <Route index element={<p>LIST</p>} />
           <Route path="cities" element={<p>List The Cities</p>} />
           <Route path="countries" element={<p>List The Countries</p>} />
           <Route path="form" element={<p>Form</p>} />
         </Route>
+
+        {/* To match all other paths which are not specified */}
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
