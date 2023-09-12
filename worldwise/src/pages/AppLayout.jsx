a05bb5dddbbe4a00458256f7ea3b1d22fdbb4417
Cleve <clevemomanyi@gmail.com>
2023-09-12 09:58:52 +0300

    app

--- worldwise/src/pages/AppLayout.jsx
+++ worldwise/src/pages/AppLayout.jsx
@@ -1,10 +1,9 @@
-import AppNav from "../components/AppNav";
+import Sidebar from "../components/Sidebar";
 
 function AppLayout() {
   return (
     <div>
-      <AppNav />
-      <p>App</p>
+      <Sidebar />
     </div>
   );
 }
