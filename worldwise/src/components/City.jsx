af04582d0afc2f86b541ab42750c7725c8251564
Cleve <clevemomanyi@gmail.com>
2023-09-14 06:40:19 +0300

    nearer still nearer

--- worldwise/src/components/City.jsx
+++ worldwise/src/components/City.jsx
@@ -1,6 +1,9 @@
-// import styles from "./City.module.css";
-
-import { useParams, useSearchParams } from "react-router-dom";
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useCities } from "../contexts/CitiesContext";
+import BackButton from "./BackButton";
+import styles from "./City.module.css";
+import Spinner from "./Spinner";
 
 const formatDate = (date) =>
   new Intl.DateTimeFormat("en", {
@@ -11,26 +14,22 @@ const formatDate = (date) =>
   }).format(new Date(date));
 
 function City() {
+  const { id } = useParams();
+  const { getCity, currentCity, isLoading } = useCities();
 
-  const [searchParams, setSearchParams] = useSearchParams();
-  const {id} = useParams();
-  const lat = searchParams.get("lat");
-  const lng = searchParams.get("lng");
-  
-  // TEMP DATA
-  // const currentCity = {
-  //   cityName: "Lisbon",
-  //   emoji: "🇵🇹",
-  //   date: "2027-10-31T15:59:59.138Z",
-  //   notes: "My favorite city so far!",
-  // };
+  useEffect(
+    function () {
+      getCity(id);
+    },
+    [id, getCity]
+  );
 
-  /// const { cityName, emoji, date, notes } = currentCity;
+  const { cityName, emoji, date, notes } = currentCity;
 
-  // return (
-  {
-    /*
-      <div className={styles.city}>
+  if (isLoading) return <Spinner />;
+
+  return (
+    <div className={styles.city}>
       <div className={styles.row}>
         <h6>City name</h6>
         <h3>
@@ -62,16 +61,10 @@ function City() {
       </div>
 
       <div>
-        <ButtonBack />
+        <BackButton />
       </div>
-      </div> 
-    */
-  }
-  return <>
-  <h3>City {id}</h3>
-  <p>Position {lat}, {lng}</p>
-  </>
-  //)
+    </div>
+  );
 }
 
 export default City;
