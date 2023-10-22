e9df9f80e7f358293d52980b0a3ef6485aa5bb7d
Cleve <clevemomanyi@gmail.com>
2023-10-22 12:35:13 +0300

    router

---
+++ 0x0f-fast-react-pizza/src/ui/Header.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom"
+
+function Header() {
+    return (
+        <header>
+            <Link to="/">Fast React Pizza Co.</Link>
+
+            <p>Cleve</p>
+        </header>
+    )
+}
+
+export default Header
