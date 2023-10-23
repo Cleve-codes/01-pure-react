308d6e69ddfb29d86bb30d8017ae033ae40fa41f
Cleve <clevemomanyi@gmail.com>
2023-10-23 14:12:07 +0300

    phone errors

--- 0x0f-fast-react-pizza/src/features/order/CreateOrder.jsx
+++ 0x0f-fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Form, redirect, useNavigation } from "react-router-dom";
+import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
 
 // https://uibakery.io/regex-library/phone-number
@@ -38,6 +38,8 @@ function CreateOrder() {
   const navigation = useNavigation();
   const isSubmitting = navigation.state === "submitting";
 
+  const formErrors = useActionData();
+
   const cart = fakeCart;
 
   return (
@@ -55,6 +57,7 @@ function CreateOrder() {
           <label>Phone number</label>
           <div>
             <input type="tel" name="phone" required />
+            {formErrors?.phone && <p>{formErrors.phone}</p>}
           </div>
         </div>
 
@@ -97,6 +100,13 @@ export async function action({ request }) {
     priority: data.priority === "on",
   };
 
+  const errors = {};
+  if (!isValidPhone(order.phone))
+    errors.phone = "Please use the correct number format";
+
+  if(Object.keys(errors).length > 0) return errors;
+
+  // If everything 
   const newOrder = await createOrder(order);
 
   return redirect(`/order/${newOrder.id}`);
