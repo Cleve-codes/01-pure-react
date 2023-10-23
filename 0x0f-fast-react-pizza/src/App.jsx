dcf46f6e8a3158c65c98dbe5506a1f5edc65de5b
Cleve <clevemomanyi@gmail.com>
2023-10-23 12:15:18 +0300

    error

--- 0x0f-fast-react-pizza/src/App.jsx
+++ 0x0f-fast-react-pizza/src/App.jsx
@@ -1,6 +1,7 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 import Home from "./ui/Home"
+import Error from "./ui/Error";
 import Menu, {loader as menuLoader } from "./features/menu/Menu"
 import Cart from './features/cart/Cart'
 import CreateOrder from "./features/order/CreateOrder";
@@ -10,6 +11,8 @@ import AppLayout from "./ui/AppLayout";
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
+    errorElement: <Error />,
+
     children: [
       {
         path: "/",
@@ -18,6 +21,7 @@ const router = createBrowserRouter([
       {
         path: "/menu",
         element: <Menu />,
+        errorElement: <Error />,
         loader: menuLoader,
       },
       {
