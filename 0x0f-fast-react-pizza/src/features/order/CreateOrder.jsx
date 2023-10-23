8b64c279ffdf01fc15712e9b8815428160aa79d9
Cleve <clevemomanyi@gmail.com>
2023-10-23 13:24:01 +0300

    cart

--- 0x0f-fast-react-pizza/src/features/order/CreateOrder.jsx
+++ 0x0f-fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -72,6 +72,7 @@ function CreateOrder() {
         </div>
 
         <div>
+          <input type="hidden" name="cart" value={JSON.stringify(cart)} />
           <button>Order now</button>
         </div>
       </Form>
@@ -82,7 +83,15 @@ function CreateOrder() {
 export async function action({request}){
   const formData = await request.formData()
   const data = Object.fromEntries(formData);
+
+  const order = {
+    ...data,
+    cart: JSON.parse(data.cart),
+    priority: data.priority === 'on',
+  }
+  
   console.log(data)
+  console.log(order)
   return null;
 }
 
