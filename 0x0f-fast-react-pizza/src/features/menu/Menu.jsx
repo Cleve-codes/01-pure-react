06f2a65a0b188f80824863a89f383cb76c8590b2
Cleve <clevemomanyi@gmail.com>
2023-10-24 10:44:55 +0300

    cart

--- 0x0f-fast-react-pizza/src/features/menu/Menu.jsx
+++ 0x0f-fast-react-pizza/src/features/menu/Menu.jsx
@@ -3,13 +3,15 @@ import { getMenu } from "../../services/apiRestaurant";
 import MenuItem from "./MenuItem";
 
 function Menu() {
-
   const menu = useLoaderData();
-  console.log(menu);
 
-  return <ul>
-    {menu.map(pizza => <MenuItem pizza={pizza} key={pizza.Id} />)}
-  </ul>;
+  return (
+    <ul className="divide-y divide-stone-200 px-2">
+      {menu.map((pizza) => (
+        <MenuItem pizza={pizza} key={pizza.id} />
+      ))}
+    </ul>
+  );
 }
 
 export async function loader() {
