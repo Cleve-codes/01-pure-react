5e9982a52708d4cf2819c35c92b2b3d1cadac04c
Cleve <clevemomanyi@gmail.com>
2023-10-29 04:16:13 +0300

    globalstyles

--- the-wild-oasis/src/App.jsx
+++ the-wild-oasis/src/App.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import GlobalStyle from "./styles/GlobalStyles";
 
 const H1 = styled.h1`
   font-size: 30px;
@@ -21,21 +22,24 @@ const Input = styled.input`
   border: 1px solid #ddd;
   border-radius: 5px;
   padding: 0.8rem 1.2rem;
-`
+`;
 
 const StyledApp = styled.div`
   background-color: orangered;
   padding: 20px;
-`
+`;
 
 function App() {
   return (
-    <StyledApp>
-      <H1>The Wild Oasis</H1>
-      <Button>Check In</Button>
-      <Button>Check Out</Button>
-      <Input type="number" placeholder="Number of guests..." />
-    </StyledApp>
+    <>
+      <GlobalStyle />
+      <StyledApp>
+        <H1>The Wild Oasis</H1>
+        <Button>Check In</Button>
+        <Button>Check Out</Button>
+        <Input type="number" placeholder="Number of guests..." />
+      </StyledApp>
+    </>
   );
 }
 
