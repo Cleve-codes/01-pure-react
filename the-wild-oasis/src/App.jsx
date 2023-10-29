ec90620cb590154909a8bcb63d1c33bce38c038e
Cleve <clevemomanyi@gmail.com>
2023-10-29 03:43:33 +0300

    styledApp component

--- the-wild-oasis/src/App.jsx
+++ the-wild-oasis/src/App.jsx
@@ -23,14 +23,19 @@ const Input = styled.input`
   padding: 0.8rem 1.2rem;
 `
 
+const StyledApp = styled.div`
+  background-color: orangered;
+  padding: 20px;
+`
+
 function App() {
   return (
-    <div>
+    <StyledApp>
       <H1>The Wild Oasis</H1>
       <Button>Check In</Button>
       <Button>Check Out</Button>
       <Input type="number" placeholder="Number of guests..." />
-    </div>
+    </StyledApp>
   );
 }
 
