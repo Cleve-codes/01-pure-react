87973db7c99e309b33150a1d263452fbd79ad601
Cleve <clevemomanyi@gmail.com>
2023-10-29 03:39:57 +0300

    reusable styled components

--- the-wild-oasis/src/App.jsx
+++ the-wild-oasis/src/App.jsx
@@ -1,17 +1,37 @@
 import styled from "styled-components";
 
+const H1 = styled.h1`
+  font-size: 30px;
+  background-color: yellow;
+  font-weight: 600;
+`;
+
+const Button = styled.button`
+  background-color: purple;
+  cursor: pointer;
+  margin: 20px;
+  color: white;
+  border: none;
+  border-radius: 10px;
+  padding: 10px;
+  height: 40px;
+`;
+
+const Input = styled.input`
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  padding: 0.8rem 1.2rem;
+`
+
 function App() {
-
-  const H1 = styled.h1`
-    font-size: 30px,
-    font-weight: 600,
-  `;
-
-  return <div>
-    <H1>
-      The Wild Oasis
-    </H1>
-  </div>;
+  return (
+    <div>
+      <H1>The Wild Oasis</H1>
+      <Button>Check In</Button>
+      <Button>Check Out</Button>
+      <Input type="number" placeholder="Number of guests..." />
+    </div>
+  );
 }
 
 export default App;
