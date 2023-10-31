8aef20143220d850caf2aaf648cd4a5edf2bfac0
Cleve <clevemomanyi@gmail.com>
2023-10-31 20:48:15 +0300

    props in styled components

--- the-wild-oasis/src/App.jsx
+++ the-wild-oasis/src/App.jsx
@@ -4,8 +4,6 @@ import Button from "./ui/Button";
 import Input from "./ui/Input";
 import Heading from "./ui/Heading";
 
-
-
 const StyledApp = styled.div`
   background-color: orangered;
   padding: 20px;
@@ -16,9 +14,11 @@ function App() {
     <>
       <GlobalStyles />
       <StyledApp>
-        <Heading>The Wild Oasis</Heading>
+        <Heading as="h1">The Wild Oasis</Heading>
+        <Heading as="h2">Checking IN</Heading>
         <Button>Check In</Button>
         <Button>Check Out</Button>
+        <Heading as="h3">Guests</Heading>
         <Input type="number" placeholder="Number of guests..." />
       </StyledApp>
     </>
