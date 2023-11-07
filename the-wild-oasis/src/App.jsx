0090d9870d8e060875262b7b2c1e0b8497b4cf13
Cleve <clevemomanyi@gmail.com>
2023-11-07 17:13:07 +0300

    UI components

--- the-wild-oasis/src/App.jsx
+++ the-wild-oasis/src/App.jsx
@@ -14,24 +14,26 @@ function App() {
     <>
       <GlobalStyles />
       <StyledApp>
-        <Row type="horizontal">
-          <Heading as="h1">The Wild Oasis</Heading>
-          <div>
-            <Heading as="h2">Checking IN</Heading>
-            <Button variation="primary" size="medium">
-              Check In
-            </Button>
-            <Button variation="secondary" size="small">
-              Check Out
-            </Button>
-          </div>
-        </Row>
-        <Row type="vertical">
-          <Heading as="h3">Guests</Heading>
-          <form>
-            <Input type="number" placeholder="Number of guests..." />
-            <Input type="number" placeholder="Number of guests..." />
-          </form>
+        <Row>
+          <Row type="horizontal">
+            <Heading as="h1">The Wild Oasis</Heading>
+            <div>
+              <Heading as="h2">Checking IN</Heading>
+              <Button variation="primary" size="medium">
+                Check In
+              </Button>
+              <Button variation="secondary" size="small">
+                Check Out
+              </Button>
+            </div>
+          </Row>
+          <Row type="vertical">
+            <Heading as="h3">Guests</Heading>
+            <form>
+              <Input type="number" placeholder="Number of guests..." />
+              <Input type="number" placeholder="Number of guests..." />
+            </form>
+          </Row>
         </Row>
       </StyledApp>
     </>
--- the-wild-oasis/src/ui/Heading.jsx
+++ the-wild-oasis/src/ui/Heading.jsx
@@ -24,7 +24,6 @@ const Heading = styled.h1`
 
   font-size: 20px;
   font-weight: 600;
-  background-color: yellow;
 `;
 
 export default Heading;
