814fb4edb6d239dac49d858fc2998337664baf10
Cleve <clevemomanyi@gmail.com>
2023-11-07 16:55:45 +0300

    styledRow

--- the-wild-oasis/src/App.jsx
+++ the-wild-oasis/src/App.jsx
@@ -16,17 +16,22 @@ function App() {
       <StyledApp>
         <Row type="horizontal">
           <Heading as="h1">The Wild Oasis</Heading>
-          <Heading as="h2">Checking IN</Heading>
-          <Button variation="primary" size="medium">
-            Check In
-          </Button>
-          <Button variation="secondary" size="small">
-            Check Out
-          </Button>
+          <div>
+            <Heading as="h2">Checking IN</Heading>
+            <Button variation="primary" size="medium">
+              Check In
+            </Button>
+            <Button variation="secondary" size="small">
+              Check Out
+            </Button>
+          </div>
         </Row>
         <Row type="vertical">
           <Heading as="h3">Guests</Heading>
-          <Input type="number" placeholder="Number of guests..." />
+          <form>
+            <Input type="number" placeholder="Number of guests..." />
+            <Input type="number" placeholder="Number of guests..." />
+          </form>
         </Row>
       </StyledApp>
     </>
